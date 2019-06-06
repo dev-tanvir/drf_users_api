d21a3d2c335e267d4f0ae9e46d807da48c3b1b1f
dev-tanvir <tanvirfaisaldev@gmail.com>
2019-06-06 22:30:28 +0600

    added custom user model manager and specifies it in settings.py

--- src/django_user_rest/django_user_rest/settings.py
+++ src/django_user_rest/django_user_rest/settings.py
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users_api.UserProfiles'      #   Telling django to use my custom user model in stead of its own
--- src/django_user_rest/users_api/models.py
+++ src/django_user_rest/users_api/models.py
@@ -5,6 +5,38 @@ from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
 
+class UserProfilesManager(BaseUserManager):
+    """ Helps Django to work with our custom user model."""
+
+    def create_user(self, email, name, password=None):
+        """ Creates a new user profile object. """
+
+        if not email:
+            raise ValueError("Users must have email address!")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        
+        user.set_password(password)     # it is used to hash the passwprd of the user
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """ Creates a new super user profile object. """
+
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+
+        
+
 class UserProfiles(AbstractBaseUser, PermissionsMixin):
     """ Represents the user profiles in the application. Extension of Django base model."""
 
