902a62d51159f6b5892869cc7c0357b8e2d23879
dev-tanvir <tanvirfaisaldev@gmail.com>
2019-06-18 03:17:44 +0600

    added Login Api updated

--- src/django_user_rest/users_api/urls.py
+++ src/django_user_rest/users_api/urls.py
@@ -5,8 +5,8 @@ from . import views
 router = DefaultRouter()
 router.register('viewsets-test', views.HelloViewsets, 'viewsets-test')
 router.register('profile', views.UserProfileViewset)    #   here, no need for base-name as 'UserProfileViewset' is a ModelViewSet
-router.register('login', views.LoginViewSet, base_name='login')     # we are using basename as
-                                                                    # 'LoginViewSet' is ModelViewSet
+router.register('login', views.LoginViewSet, 'login')     # we need to use basename as
+                                                                    # 'LoginViewSet' is not ModelViewSet
 
 urlpatterns = [
     path('apiview-test/', views.HelloAPIView.as_view(), name='Apiview Test'),
