7a707bc95949527f4a3cd193afc7b37e0881cdbd
dev-tanvir <tanvirfaisaldev@gmail.com>
2019-06-21 14:05:07 +0600

    added viewset to the router for url matching

--- src/django_user_rest/users_api/urls.py
+++ src/django_user_rest/users_api/urls.py
@@ -5,8 +5,9 @@ from . import views
 router = DefaultRouter()
 router.register('viewsets-test', views.HelloViewsets, 'viewsets-test')
 router.register('profile', views.UserProfileViewset)    #   here, no need for base-name as 'UserProfileViewset' is a ModelViewSet
-router.register('login', views.LoginViewSet, 'login')     # we need to use basename as
-                                                                    # 'LoginViewSet' is not ModelViewSet
+router.register('login', views.LoginViewSet, 'login')   # we need to use basename as
+                                                        # 'LoginViewSet' is not ModelViewSet
+router.register('status', views.ProfileStatusViewset)
 
 urlpatterns = [
     path('apiview-test/', views.HelloAPIView.as_view(), name='Apiview Test'),
