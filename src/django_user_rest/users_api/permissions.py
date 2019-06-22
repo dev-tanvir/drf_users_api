8a78176493bd2f9e1b7eaed2ebda9e16d9b66a1a
dev-tanvir <tanvirfaisaldev@gmail.com>
2019-06-22 13:31:59 +0600

    added permissions class for post status

--- src/django_user_rest/users_api/permissions.py
+++ src/django_user_rest/users_api/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 
 class UpdateOwnProfile(permissions.BasePermission):
-    """ Allow user to update his profile only."""
+    """ Allow user to update own profile only."""
 
     def has_object_permission(self, request, view, obj):
         """ Checking if user is modifying his own profile."""
@@ -10,3 +10,15 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
+
+
+class PostOwnStatus(permissions.BasePermission):
+    """Allow user to post status on own profile only. """
+
+    def has_object_permission(self, request, view, obj):
+        """ This is to check if user is posting/modifying own status."""
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
